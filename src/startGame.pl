207f17dd6240612d7bf37c5cc4121dfa12b91431
Ghaniyul Amri Caulava <ghanivivo123@gmail.com>
2026-05-18 19:39:03 +0700

    Update startGame.pl

--- src/startGame.pl
+++ src/startGame.pl
@@ -13,16 +13,51 @@ reset_game :-
 /*penggunaan random*/
 acak_list([], []).
 acak_list(ListAwal, [ElemenTerpilih | SisaDiacak]) :-
-    length(ListAwal, Panjang),
+    get_length(ListAwal, Panjang),
     random(0, Panjang, IndeksAcak),
-    ambil_elemen_ke(IndeksAcak, ListAwal, ElemenTerpilih, SisaList),
+    get_element(IndeksAcak, ListAwal, ElemenTerpilih, SisaList),
     acak_list(SisaList, SisaDiacak).
 
-ambil_elemen_ke(0, [H|T], H, T).
-ambil_elemen_ke(N, [H|T], Elemen, [H|Sisa]) :-
+/* get_element: ambil elemen ke-N dan kembalikan sisa list */
+get_element(0, [H|T], H, T).
+get_element(N, [H|T], Elemen, [H|Sisa]) :-
     N > 0,
     N1 is N - 1,
-    ambil_elemen_ke(N1, T, Elemen, Sisa).
+    get_element(N1, T, Elemen, Sisa).
+
+/* get_length: hitung panjang list */
+get_length([], 0).
+get_length([_|T], N) :-
+    get_length(T, N1),
+    N is N1 + 1.
+
+/* ambil_n_elemen: ambil N elemen pertama dari list */
+ambil_n_elemen(0, Sisa, [], Sisa).
+ambil_n_elemen(N, [H|T], [H|Hasil], Sisa) :-
+    N > 0,
+    N1 is N - 1,
+    ambil_n_elemen(N1, T, Hasil, Sisa).
+
+/* cek_member: cek apakah elemen ada dalam list */
+cek_member(X, [X|_]).
+cek_member(X, [_|T]) :-
+    cek_member(X, T).
+
+/* tidak_ada_dalam: cek apakah elemen TIDAK ada dalam list */
+tidak_ada_dalam(_, []).
+tidak_ada_dalam(X, [H|T]) :-
+    X \= H,
+    tidak_ada_dalam(X, T).
+
+/* gabung_list: gabungkan dua list menjadi satu */
+gabung_list([], L, L).
+gabung_list([H|T], L, [H|Hasil]) :-
+    gabung_list(T, L, Hasil).
+
+/* tambah_akhir: tambahkan elemen ke akhir list */
+tambah_akhir([], X, [X]).
+tambah_akhir([H|T], X, [H|Hasil]) :-
+    tambah_akhir(T, X, Hasil).
 
 /*pembuatan deck*/
 buat_deck(ShuffledDeck) :-
@@ -32,15 +67,15 @@ buat_deck(ShuffledDeck) :-
                     [skip, reverse, draw_two], DeckAksi),
     kombinasi_kartu_wild([hitam], 
                          [wild, wild_draw_four], DeckWild),
-    append(DeckAngka, DeckAksi, TempDeck),
-    append(TempDeck, DeckWild, DeckAwal),
+    gabung_list(DeckAngka, DeckAksi, TempDeck),
+    gabung_list(TempDeck, DeckWild, DeckAwal),
     acak_list(DeckAwal, ShuffledDeck).
 
 kombinasi_kartu([], _, []).
 kombinasi_kartu([W | Ws], ListJenis, Deck) :-
     pasangkan_satu_warna(W, ListJenis, KartuWarnaIni),
     kombinasi_kartu(Ws, ListJenis, SisaDeck),
-    append(KartuWarnaIni, SisaDeck, Deck).
+    gabung_list(KartuWarnaIni, SisaDeck, Deck).
 
 pasangkan_satu_warna(_, [], []).
 pasangkan_satu_warna(Warna, [J | Js], [kartu(Warna, J) | SisaKartu]) :-
@@ -50,30 +85,29 @@ kombinasi_kartu_wild([], _, []).
 kombinasi_kartu_wild([W | Ws], ListJenis, Deck) :-
     pasangkan_wild_4x(W, ListJenis, KartuWildIni),
     kombinasi_kartu_wild(Ws, ListJenis, SisaDeck),
-    append(KartuWildIni, SisaDeck, Deck).
+    gabung_list(KartuWildIni, SisaDeck, Deck).
 
 pasangkan_wild_4x(_, [], []).
 pasangkan_wild_4x(Warna, [J | Js], [kartu(Warna, J), kartu(Warna, J), kartu(Warna, J), kartu(Warna, J) | SisaKartu]) :-
     pasangkan_wild_4x(Warna, Js, SisaKartu).
 
 /*inisialisasi pemain*/
-baca_nama(NamaFinal) :-
-    read_term(Input, [variable_names(Vars)]),
-    (   Vars = [NamaKapital = _] -> 
-        NamaFinal = NamaKapital 
-    ;   
-        NamaFinal = Input       
-    ).
+
+/* baca_nama: baca input nama sebagai atom */
+baca_nama(Nama) :-
+    read(Nama).
 
 inisialisasi_jumlah_pemain(Jumlah) :-
     write('Masukkan jumlah pemain: '),
     read(Input),
     validasi_jumlah(Input, Jumlah).
 
-validasi_jumlah(Input, Input) :-
-    integer(Input), Input >= 2, Input =< 4, !.
-
-validasi_jumlah(_, Jumlah) :-
+/* validasi_jumlah: hanya menerima integer 2-4 */
+validasi_jumlah(2, 2).
+validasi_jumlah(3, 3).
+validasi_jumlah(4, 4).
+validasi_jumlah(Input, Jumlah) :-
+    Input \= 2, Input \= 3, Input \= 4,
     write('Mohon masukkan angka antara 2 - 4.'), nl,
     inisialisasi_jumlah_pemain(Jumlah).
 
@@ -82,15 +116,15 @@ inisialisasi_nama_pemain(N, Jumlah, Acc, Acc) :-
 
 inisialisasi_nama_pemain(N, Jumlah, Acc, ListPemain) :-
     N =< Jumlah,
-    format('Masukkan nama pemain ~w: ', [N]),
-    baca_nama(Nama), % <--- Menggunakan baca_nama, bukan read lagi
+    write('Masukkan nama pemain '), write(N), write(': '),
+    baca_nama(Nama),
     validasi_nama(Nama, Acc, NamaValid),
-    append(Acc, [NamaValid], NewAcc),
+    tambah_akhir(Acc, NamaValid, NewAcc),
     NextN is N + 1,
     inisialisasi_nama_pemain(NextN, Jumlah, NewAcc, ListPemain).
 
 validasi_nama(Nama, Acc, Nama) :-
-    \+ member(Nama, Acc), !.
+    tidak_ada_dalam(Nama, Acc), !.
 
 validasi_nama(_, Acc, NamaValid) :-
     write('Nama sudah digunakan. Masukkan nama lain: '),
@@ -107,8 +141,7 @@ cetak_urutan([H|T]) :-
 bagi_kartu_pemain([], Deck, Deck).
 
 bagi_kartu_pemain([Pemain | SisaPemain], DeckSekarang, SisaDeck) :-
-    length(Tangan, 7), 
-    append(Tangan, DeckBaru, DeckSekarang), 
+    ambil_n_elemen(7, DeckSekarang, Tangan, DeckBaru),
     assertz(kartu_pemain(Pemain, Tangan)),
     bagi_kartu_pemain(SisaPemain, DeckBaru, SisaDeck).
 
@@ -117,8 +150,7 @@ tentukan_kartu_awal([kartu(W, J) | SisaDeck], SisaDeck, kartu(W, J)) :-
     jenis_angka(J), !. 
 
 tentukan_kartu_awal([kartu(W, J) | SisaDeck], DeckFinal, KartuAwal) :-
-    \+ jenis_angka(J), 
-    append(SisaDeck, [kartu(W, J)], DeckSementara), 
+    tambah_akhir(SisaDeck, kartu(W, J), DeckSementara),
     tentukan_kartu_awal(DeckSementara, DeckFinal, KartuAwal). 
 
 /*start game*/
@@ -144,10 +176,10 @@ startGame :-
     assertz(discard_pile([kartu(WarnaTop, JenisTop)])),
     assertz(deck(SisaTumpukanDraw)),
     
-    format('Kartu discard top: ~w-~w.', [WarnaTop, JenisTop]), nl, nl,
+    write('Kartu discard top: '), write(WarnaTop), write('-'), write(JenisTop), write('.'), nl, nl,
     
     UrutanPemain = [PemainPertama | _],
     assertz(giliran(PemainPertama)),
     assertz(permainan_aktif),
     
-    format('Giliran ~w.', [PemainPertama]), nl.
+    write('Giliran '), write(PemainPertama), write('.'), nl.
